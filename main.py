243ae8e9218fa763f2cd24b068e0df6f36fa02f1
Jr <87111680+AntonyKoo@users.noreply.github.com>
2022-01-26 00:59:31 +0900

    coffeemachine_v2
    수정 version

--- main.py
+++ main.py
@@ -32,35 +32,16 @@ resources = {
 }
 
 
-def is_resource_sufficient(order_ingredients):
-    """Returns True when order can be made, False if ingredients are insufficient."""
-    for item in order_ingredients:
-        if order_ingredients[item] >= resources[item]:
-            print(f"Sorry there is not enough {item}.")
-            return False
-    return True # sufficient 하다면, True 임.
-
-
-def process_coins():
-    """Returns the total calculated from coins inserted."""
-    print("please insert coins.")
-    total = int(input("how many quarters?: ")) * 0.25
-    total += int(input("how many dimes?: ")) * 0.1
-    total += int(input("how many nickles?: ")) * 0.05
-    total += int(input("how many pennies?: ")) * 0.01
-    return total
-
-
 def is_transaction_successful(money_received, drink_cost):
     """Return True when the payment is accepted, or False if money is insufficient."""
     if money_received >= drink_cost:
-        change = round(money_received - drink_cost, 2)
+        change = round(money_received - drink_cost, 2)  # round 소수점 계산
         print(f"Here is ${change} in charge.")
         global profit
         profit += drink_cost
         return True
     else:
-        print("Sorry that's not enough money. Money refunded.")
+        print("Sorry, that's not enough money. Money refunded.")
         return False
 
 
@@ -71,7 +52,27 @@ def make_coffee(drink_name, order_ingredients):
     print(f"Here is your {drink_name}☕. Enjoy!")
 
 
+
+def process_coins():
+    """Return the total calculated from coins inserted."""
+    print("Please, insert coins.")
+    total = int(input("how many quarters?:")) * 0.25
+    total += int(input("how many dimes?:")) * 0.1
+    total += int(input("how many nickles?:")) * 0.05
+    total += int(input("how many pennies?:")) * 0.01
+    return total
+
+
+def is_resource_sufficient(order_ingredients):  # 이 함수는 T/F를 return 하는 함수
+    for item in order_ingredients:  # for loop에 사전형을 담으면, 개별 인자는 key가 담긴다.
+        if order_ingredients[item] > resources[item]:  ### >=가 아닌게 맞지?
+            print(f"Sorry, there is not enough {item}.")
+            return False
+    return True  # if 구문이 작동하지 않는 경우.
+
+
 is_on = True
+
 
 while is_on:
     choice = input("What would you like? (espresso/latte/cappuccino):")
@@ -82,10 +83,11 @@ while is_on:
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
         print(f"Money: ${profit}")
-    else:    # 음료의 이름을 입력 했을 때
-        drink = MENU[choice]
+    elif choice not in ["latte", "espresso", "cappuccino"]:
+        pass
+    else:
+        drink = MENU[choice]  # key가 "ingredient", "cost"인 dictionary data
         if is_resource_sufficient(drink["ingredients"]):
-            payment = process_coins()
+            payment = process_coins()  # inserted coins의 total을 알았으니, 돈계산 해야 함.
             if is_transaction_successful(payment, drink["cost"]):
                 make_coffee(choice, drink["ingredients"])
-
